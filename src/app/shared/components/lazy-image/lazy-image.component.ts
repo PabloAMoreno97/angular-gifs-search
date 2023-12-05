77cc90fb205b24fd17f28ca31fcedbb7a2eedb6b
Pablo Moreno <pablolm97@gmail.com>
2023-12-05 10:34:19 -0500

    Adding a manual lazy-image

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyimageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL Property is required');
+  }
+
+  public onLoad() {
+    setTimeout(() =>{
+      this.hasLoaded = true;
+    }, 1000);
+  }
+
+}
